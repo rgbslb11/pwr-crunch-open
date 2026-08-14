f399cc6b6bfc422bf61b09959c1c473fb9177045
rgbslb11 <realgbax@gmail.com>
2026-08-13 21:55:04 -0500

    Add scoped preview service worker

---
+++ preview-v2.0.1/service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='power-crunch-v2.0.1-preview-1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./board_ik.js','./ratings_v201.js','../style.css','../teams.js','../conference.js','../app.js'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('power-crunch-v2.0.1-preview-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
